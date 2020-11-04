0d2623c16ccdc7f55124f033b5e25ee2b67a6687
trungshin <41856824+trungshin@users.noreply.github.com>
2020-11-04 09:11:22 +0700

    Add files via upload

---
+++ configs/config_DB.js
@@ -0,0 +1,16 @@
+module.exports = {
+    localdb: {
+        urldb : 'mongodb://localhost:27017/shopsmall',
+        dbname : "shopsmall",
+        dbusername : "",
+        dbpassword : "",
+    },
+
+    clouddb: {
+        urldb : 'mongodb://localhost:27017/shopsmall',
+        dbname : "shopsmall",
+        dbusername : "",
+        dbpassword : "",
+    }
+
+};

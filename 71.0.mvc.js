5cef4a5b885bf135c6e54c9304568970f6fa35b8
trungshin <41856824+trungshin@users.noreply.github.com>
2020-11-07 07:08:47 +0700

    Update 71.0.mvc.js

--- 71.0.mvc.js
+++ 71.0.mvc.js
@@ -357,7 +357,7 @@ function qrPage(req, res) {
 /// ------------------ gọi SERVER thực thi
 
 
-var server = app.listen( PORT , function () {
+var server = app.listen( process.env.PORT || 8080 , function () {
    var host = server.address().address
    var port = server.address().port
    
